de7d85122e91cfcb2f45ce0c92e86829c012956a
Gerald Orin Tucker Jr <got150030@cs6360.utdallas.edu>
2017-03-28 21:48:47 -0500

    Finished initial setup

--- pspc.scala
+++ pspc.scala
@@ -15,6 +15,26 @@ val parts = line.split(',')
 LabeledPoint(parts(3).toDouble, Vectors.dense(parts.dropRight(1).map(_.toDouble)))
 }
 
+def calcAccuracy(loopCount:Int, train:Double, test:Double)={
+  val split = Array(train,test)
+  var results : List[Double] = List() 
+  
+  for (i <- 1 to loopCount) yield {
+    val seed = System.currentTimeMillis()
+    val splits = parsedData.randomSplit(split, seed)
+    val training = splits(0)
+    val test = splits(1)
+    val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
+    val predictionAndLabel = test.map(p=> (model.predict(p.features),p.label))
+    val accuracy :Double = 1.0 * predictionAndLabel.filter(x=> x._1 == x._2).count() / test.count()
+    val result = accuracy
+    results :::= List(accuracy)
+    Console.print(i + "..")
+  }
+
+  Console.println("\n*** Average Results [" + loopCount + " runs | " + train + " / " + test + "]: " + (results.sum/loopCount))
+}
+
 calcAccuracy(50,.7,.3)
 calcAccuracy(50,.8,.2)
 calcAccuracy(50,.9,.1)
