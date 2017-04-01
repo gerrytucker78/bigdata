2aa515c70560b04f2574428cfee906a3104ddb3c
Gerald Orin Tucker Jr <got150030@cs6360.utdallas.edu>
2017-04-01 12:24:56 -0500

    New data mixes

--- pspic.scala
+++ pspic.scala
@@ -12,7 +12,7 @@ val data = sc.textFile("/got150030/project/p_sp_i_c_cd.csv")
 
 val parsedData = data.map {line=>
 val parts = line.split(',')
-LabeledPoint(parts(3).toDouble, Vectors.dense(parts.dropRight(1).map(_.toDouble)))
+LabeledPoint(parts(4).toDouble, Vectors.dense(parts.dropRight(1).map(_.toDouble)))
 }
 
 def calcAccuracy(loopCount:Int, train:Double, test:Double)={
